9a717fadf2361422cbdf08ddfd12b18179bfca79
Jeth0214 <rolandjethrosuyom@yahoo.com>
2020-04-03 19:08:30 +0300

    mark-up and styling

---
+++ calc.js
@@ -0,0 +1,6 @@
+const numberButtons = document.querySelectorAll('.numberButtons');
+const operators = document.querySelectorAll('.operators');
+const reset = document.querySelector('#clear');
+const equals = document.querySelector('#equals');
+const historyText = document.querySelector('#history');
+const displayText = document.querySelector('#display');
